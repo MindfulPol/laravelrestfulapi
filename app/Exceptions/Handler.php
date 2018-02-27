217982184a7c44befdfc7d5f49078e75610322cb
Pol Lluis <plluis@epic.es>
2018-02-27 13:33:11 +0100

    handler updated

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -48,6 +48,6 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        return response()->json(['error' => 'Paràmetres de la query incorrectes'], 405);
     }
 }
